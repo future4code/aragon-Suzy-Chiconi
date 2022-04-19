e7506438023aaff95ed24b2a77d92d4eac60f9f8
Suzy Assis Chiconi <suzy.chiconi@gmail.com>
2022-04-18 21:26:10 -0300

    jsx-labetube

---
+++ modulo2/jsx-labetube/labetube-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* LI DIVERSAS VEZES OS TÓPICOS PARA O DESENVOLVIMENTO DA TAREFA, MAS NÃO CONSEGUI FAZER NADA, NEM MESMO RODAR O REACT.
+      FICAREI DEVENDO ESSA TAREFA - AMANHÃ TENTO ENTENDER COMO FAZER, COM A RESOLUÇÃO. */}
+    </div>
+  );
+}
+
+export default App;
