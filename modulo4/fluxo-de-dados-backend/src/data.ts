4292d00e04dbc65186f80f00f0cf724162270ae8
Suzy Assis Chiconi <suzy.chiconi@gmail.com>
2022-07-06 20:42:03 -0300

    fluxo-de-dados-backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,48 @@
+export type Produto = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export let produtos: Produto[] = [
+    {
+        id: "1",
+        name: "sofá",
+        price: 1399.99
+    },
+    {
+        id: "2",
+        name: "banqueta",
+        price: 199.99
+    },
+    {
+        id: "3",
+        name: "rack",
+        price: 599.99
+    },
+    {
+        id: "4",
+        name: "mesa com 4 cadeiras",
+        price: 749.99
+    },
+    {
+        id: "5",
+        name: "guarda-roupa 6 portas",
+        price: 1759.99
+    },
+    {
+        id: "6",
+        name: "cama box casal",
+        price: 899.99
+    },
+    {
+        id: "7",
+        name: "cama box solteiro",
+        price: 499.99
+    },
+    {
+        id: "8",
+        name: "cômoda 4 gavetas",
+        price: 319.99
+    },
+]
